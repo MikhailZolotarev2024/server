a2c57da869962a3b53c142ad538de1175dc381db
MikhailZolotarev2024 <mikhailzolotarev2018@gmail.com>
2025-04-30 12:37:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.get('/wallet/:address', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
